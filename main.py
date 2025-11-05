36a68d2f3cd049a0d2c1f94eeb7cfb6908660250
sibongiseni <sibongiseni.gmail.com>
2025-11-05 12:57:36 +0200

    fixing my errors

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import openai
 
-open.api_key = "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"
+openai.api_key = "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"
 
 def chat_with_Chatbot(prompt):
-    response = openai.chatCompletion.create(
-        model = "gpt-3.5=turbo",
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
         messages = [{"role": "user", "content": prompt}]
     )
 
     return response.choices[0].message.content.strip()
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     while True:
         user_input = input("You: ")
         if user_input.lower() in  ["quit", "exit", "bye"]:
