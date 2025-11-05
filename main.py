1803ad76fe589d7a1eaab3a141dca3d2ab3744fc
sibongiseni <sibongiseni.gmail.com>
2025-11-05 12:50:44 +0200

    Creating a chatbot

---
+++ main.py
@@ -0,0 +1,20 @@
+import openai
+
+open.api_key = "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"
+
+def chat_with_Chatbot(prompt):
+    response = openai.chatCompletion.create(
+        model = "gpt-3.5=turbo",
+        messages = [{"role": "user", "content": prompt}]
+    )
+
+    return response.choices[0].message.content.strip()
+
+if _name_ == "_main_":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in  ["quit", "exit", "bye"]:
+            break
+        
+        response  = chat_with_Chatbot(user_input)
+        print("Chatbot: ", response)
