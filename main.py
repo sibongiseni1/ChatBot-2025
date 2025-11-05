09f4aab3cf7f6198826362ad2feca27add66fd88
sibongiseni <sibongiseni.gmail.com>
2025-11-05 14:30:15 +0200

    creating my second bot from scratch

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-import openai
+import os
+from openai import OpenAI
 
-openai.api_key = "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"
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-def chat_with_Chatbot(prompt):
-    response = openai.ChatCompletion.create(
+response = client.chat.completions.create(
         model = "gpt-3.5-turbo",
-        messages = [{"role": "user", "content": prompt}]
+        messages = [{"role": "user", "content": "What is Capital of South Africa"}]
     )
 
-    return response.choices[0].message.content.strip()
-
-if __name__ == "__main__":
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() in  ["quit", "exit", "bye"]:
-            break
-        
-        response  = chat_with_Chatbot(user_input)
-        print("Chatbot: ", response)
+response_message = response.choices[0].message.content
+print(response_message)
